ee72cf0ed21d0e013227eee138c73c365356cd0f
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-10-19 19:50:03 +0200

    feat: project cleanup

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-# Create an Azure Resource Group
-resource "azurerm_resource_group" "rg_region_eastus" {
-  name     = "rg-dev-corp-app1-eastus"
-  location = "East US"
-  tags = {
-    environment = "Development"
-  }
-}
